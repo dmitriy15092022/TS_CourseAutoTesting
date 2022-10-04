23bc138916abfbf0cd9d206be91509bc3617a125
dmitriy15092022 <dmitriy15092022@gmail.com>
2022-10-04 19:46:36 +0300

    working at task_2 1_19:45_04102022

--- HomeWork/HomeWork_5/task_2.ts
+++ HomeWork/HomeWork_5/task_2.ts
@@ -2,21 +2,10 @@ let credit: number;
 let pereplata: number;
 
 function getPereplata(credit: number) {
-    const stavka: number = 17 / 12;
-    const period: number = 5 * 12;
-    let platej1: number;
-    let platej2: number;
-    let platej3: number;
-    const koef1: number = Math.pow(1 + stavka, period);
+    const stavka = 17;
+    const period = 5;
 
-    // eslint-disable-next-line prefer-const
-    platej1 = koef1 - 1;
-    // eslint-disable-next-line prefer-const
-    platej2 = stavka / koef1;
-    // eslint-disable-next-line prefer-const
-    platej3 = stavka + platej2;
-    console.log(platej1);
-    pereplata = credit * platej3 * period - credit;
+    pereplata = credit - ((credit * stavka) / 100) * period;
     return pereplata;
 }
 
