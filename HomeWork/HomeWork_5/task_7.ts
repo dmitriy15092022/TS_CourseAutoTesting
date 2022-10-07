36fd722967b7bc6669b403aa74374bf9a6c5436c
Dmitriy <dmitriy15092022@gmail.com>
2022-10-07 07:04:29 +0300

    correcting task_7

--- HomeWork/HomeWork_5/task_7.ts
+++ HomeWork/HomeWork_5/task_7.ts
@@ -1,10 +1,8 @@
 let result7 = 1;
 
-function fact(a7: number) {
+((a7: number) => {
     while (a7) {
         result7 *= a7--;
     }
-    return result7;
-}
-
-console.log(fact(10));
+    console.log(result7);
+})(10);
